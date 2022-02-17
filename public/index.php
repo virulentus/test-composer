3e5b5061a6d2081ea501465f69a0986a3929d787
virulentus <vasilievva@hotmail.com>
2022-02-17 13:19:38 +0200

    refactor

---
+++ public/index.php
@@ -0,0 +1,29 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$loader = new FilesystemLoader('templates');
+$view = new Environment($loader);
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args) use ($view) {
+    $body = $view->render('index.twig');
+    $response->getBody()->write($body);
+    return $response;
+});
+
+$app->get('/about', function (Request $request, Response $response, $args) use ($view) {
+    $body = $view->render('about.twig', [
+        'name' => 'Max'
+    ]);
+    $response->getBody()->write($body);
+    return $response;
+});
+
+$app->run();
